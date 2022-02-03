07dcc0999c85286d0cecb73cd5802abe626f6334
Tihon Pavlov <f1ssur4@Tihons-Air.N4>
2022-02-03 15:42:12 +0200

    migrate

---
+++ database/migrations/2022_02_03_124949_create_catalog_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatalogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalog', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->integer('id_brand');
+            $table->integer('id_category');
+            $table->integer('id_availability:');
+            $table->double('price');
+            $table->foreignId('id_currency')->constrained('catalog_currencies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalog');
+    }
+}
